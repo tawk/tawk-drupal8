6aefbc382d934bf6a9b9ec7345bfbd42a2c69bf7
Alvin Johnson So <so.alvinjohnson@gmail.com>
2021-04-08 19:56:03 +0800

    added platform query param when loading iframe

--- tawk_to/src/core/TawktoGenerator.php
+++ tawk_to/src/core/TawktoGenerator.php
@@ -339,7 +339,7 @@ class TawktoGenerator
         </div>
         <script>
         var currentHost = window.location.protocol + "//" + window.location.host;
-        var url = "<?php echo $iframeUrl; ?>&parentDomain=" + currentHost;
+        var url = "<?php echo $iframeUrl; ?>&platform=drupal8&parentDomain=" + currentHost;
 
         jQuery("#tawkIframe").attr("src", url);
 
